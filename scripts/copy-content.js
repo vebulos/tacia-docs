b7521808ff3bdfd25808dc01bb53b3f7e1e440a4
Vladimir Buls <vlad.buls@gmail.com>
2025-06-10 17:33:45 +0200

    Modified copy-content script to purge the destination folder before copying the content in

--- scripts/copy-content.js
+++ scripts/copy-content.js
@@ -7,6 +7,9 @@ const destDir = path.join(__dirname, '../src/assets/content');
 // Ensure the destination directory exists
 fs.ensureDirSync(destDir);
 
+// Remove all files and subdirectories in the destination directory
+fs.emptyDirSync(destDir);
+
 // Copy the content directory
 fs.copySync(srcDir, destDir, { overwrite: true });
 
