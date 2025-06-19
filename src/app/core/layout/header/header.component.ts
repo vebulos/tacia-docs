ff9d9a8018217b9580f2faf08989bd8095c8d12e
Vladimir Buls <vlad.buls@gmail.com>
2025-06-19 22:01:34 +0200

    Extend the hader to match the width of the main area

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   template: `
     <header class="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800 sticky top-0 z-20 h-16">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16">
+      <div class="w-full mx-auto px-4 sm:px-6 lg:px-8 h-16">
         <div class="flex justify-between items-center h-16">
           <div class="flex items-center">
             <a routerLink="/" class="flex items-center">
